98f3b7d1e2f7a7ac50c00c1e08665afd677edb97
Tristan Ramirez <tristan.rami@gmail.com>
2020-10-23 10:14:50 +0200

    chore: implement service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import { App } from './app'
 import './i18n'
 import './index.scss'
+import * as serviceWorker from './serviceWorker'
 
 global.APP_VERSION = process.env.REACT_APP_VERSION
 global.APP_NAME = process.env.REACT_APP_NAME
@@ -11,4 +12,14 @@ const mountingPoint = document.getElementById(
   process.env.REACT_APP_MOUNTING_POINT_ID,
 )
 
-ReactDOM.render(<App />, mountingPoint)
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  mountingPoint,
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister()
