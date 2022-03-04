948888894fbf498d5b71be9143b32982225f45a9
Tristan Ramirez <tristan.rami@gmail.com>
2022-03-04 20:49:07 +0100

    chore: enhance linter

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   semi: false,
-  trailingComma: 'all',
-  arrowParens: 'avoid',
+  trailingComma: "all",
+  arrowParens: "avoid",
   singleQuote: false,
   printWidth: 80,
-  proseWrap: 'preserve',
+  proseWrap: "preserve",
   tabWidth: 2,
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,7 @@ import { useTranslation } from "react-i18next"
 import { PageTitle } from "../../components"
 import { SEO } from "../../layout/SEO"
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default function Home(props: RouteComponentProps) {
   const { t } = useTranslation()
 
